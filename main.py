10e30590231744128784c26696283e120ac67404
Ubermensch <34875969+CallMeUber@users.noreply.github.com>
2021-05-13 17:04:35 -0500

    Separated auth token from config file + minor code refactor

--- main.py
+++ main.py
@@ -13,16 +13,26 @@ def get_config():
         return None
 
 
+def get_auth_token():
+    try:
+        with open('./authToken.yml') as file:
+            return yaml.full_load(file)['auth_token']
+    except IOError:
+        print("Could not read the file")
+        return None
+
+
 @client.event
 async def on_message(message):
 
     global gonk_flag,choom_flag,birthday_list
 
+    copy_pasta = "I’ll have you cognize I graduated most elite in my division in the Navy Seals, and I’ve been enmeshed in multifarious undisclosed incursions on Al-Quaeda, and I have over 300 habituated executions. I am experienced in guerilla campaign and I’m the most qualigied sharpshooter in the full US armed forces. You are nothing to me but just an additional victim. I will obliterate you the copulate out with rigor the likes of which has at no time been seen previously on this macrocosm, indicate my fricking units of language. You assume you can be acquit with saying that fecal matter to me via the Information Superhighway? Acknowledge anew, undesirable person. At the time that we communicate I am influencing my covert organization of operatives crosswise the New World and your Internet Protocol is being pursued immediately so you better bolster for the assault, larval fly. The disturbance that annihilates the deplorable petty existence you refer to as your duration. You’re frigging late, child under the age of 18. I can be omnipresent, at any unspecified point in time, and I can annihilate you in surplus seven centum techniques, and that’s merely with my unequipped metacarpus. Not exclusively am I extensively qualified for hand-to-hand combat, but I have admittance to the total armory of the United States Marine Corps and I will handle it to its absolute breadth to decimate your wrethed derriere off the surface of the large landmass, you minuscule excrement. If only you could have been aware of what unhallowed comeuppance your smol “ingenious” statement was almost to bear downward upon you, perchance you bequest have refrained from unpleasantly speaking. However, you couldn’t, you didn’t, and now you’re reconciling, you accursed tomfool. I will excrete acrimony all over you and you will inundate in it. You’re flipping finito"
+
     content = message.content.lower()
 
     if content == "uber gay":
-        await message.channel.send(
-            "What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo. ")
+        await message.channel.send(copy_pasta)
 
     elif content == "!stop gonk":
         gonk_flag = False
@@ -37,9 +47,8 @@ async def on_message(message):
         choom_flag = True
 
     elif "!birthday" in content:
-        negator = content[10:13]
-        print(f'negator: {negator}')
-        if negator == "not":
+        negator_string = content[10:13]
+        if negator_string == "not":
             person = content[14:]
             if person in birthday_list:
                 birthday_list.remove(person)
@@ -47,7 +56,6 @@ async def on_message(message):
             person = content[10:]
             if person not in birthday_list:
                 birthday_list.append(person)
-            print(person)
 
     author_name = message.author.name.lower()
     author_discriminator = message.author.discriminator
@@ -84,6 +92,7 @@ async def react_choom(message):
 
 
 def main():
+    token = get_auth_token()
     config = get_config()
 
     if config is None:
@@ -96,7 +105,6 @@ def main():
     choom_list = config['chooms']
     gonk_list = config['gonks']
     birthday_list = config['birthdayBoy'] if config['birthdayBoy'] else []
-    token = config['auth_token']
 
     client.run(token)
 
